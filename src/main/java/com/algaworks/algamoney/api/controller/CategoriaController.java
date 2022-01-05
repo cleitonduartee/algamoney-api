64cdf304e943d805db2c08c890322bbe1d4eb0a1
cleitonduartee <djcleitonduarte@hotmail.com>
2022-01-04 23:11:45 -0400

    Criado rota que busca por Codigo

--- src/main/java/com/algaworks/algamoney/api/controller/CategoriaController.java
+++ src/main/java/com/algaworks/algamoney/api/controller/CategoriaController.java
@@ -1,2 +1,44 @@
-package com.algaworks.algamoney.api.controller;public class CategoriaController {
+package com.algaworks.algamoney.api.controller;
+
+import com.algaworks.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.api.repository.CategoriaRepository;
+import com.sun.jndi.toolkit.url.Uri;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> buscarTodos(){
+        return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+        categoria = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoria.getCodigo()).toUri();
+
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoria);
+    }
+
+    @GetMapping("/{codigo}")
+    public Categoria buscarPorCodigo(@PathVariable Integer codigo){
+        return categoriaRepository.findById(codigo).get();
+    }
 }
--- src/main/java/com/algaworks/algamoney/api/model/Categoria.java
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -1,2 +1,22 @@
-package com.algaworks.algamoney.api.model;public class Categoria {
+package com.algaworks.algamoney.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigo;
+    private String nome;
+
+
 }
